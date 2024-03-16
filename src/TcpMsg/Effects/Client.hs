7bb2af081ae0ef977cbdd87b612f376f1acc793e
Maciej <wasilewski.maciej20@gmail.com>
2024-03-16 20:10:11 +0100

    typing

--- src/TcpMsg/Effects/Client.hs
+++ src/TcpMsg/Effects/Client.hs
@@ -6,13 +6,17 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RankNTypes #-}
 
 module TcpMsg.Effects.Client where
 
-import qualified Data.HashTable.IO as H
+
+import qualified Data.HashTable.ST.Basic as H
 import Data.Serialize (Serialize)
 import qualified Data.Text as T
 import Data.UnixTime (UnixTime (UnixTime), getUnixTime)
+
 import Effectful
   ( Dispatch (Static),
     DispatchOf,
@@ -21,8 +25,8 @@ import Effectful
     (:>),
   )
 import Effectful.Concurrent (Concurrent)
-import Effectful.Concurrent.Async (Async)
-import Effectful.Concurrent.MVar (MVar)
+import Effectful.Concurrent.Async (Async, async)
+import Effectful.Concurrent.MVar (MVar, takeMVar)
 import Effectful.Dispatch.Static
   ( SideEffects (WithSideEffects),
     StaticRep,
@@ -31,12 +35,13 @@ import Effectful.Dispatch.Static
   )
 import TcpMsg.Data (UnixMsgTime, fromUnix, mkMsg)
 import TcpMsg.Effects.Connection (Conn, write)
+import Control.Monad.ST (runST)
 
 ----------------------------------------------------------------------------------------------------------
 
-type MessageMap response = H.BasicHashTable UnixMsgTime (MVar response)
+type MessageMap response = forall s. H.HashTable s UnixMsgTime (MVar response)
 
-data ClientState a b = ClientActions
+data ClientState a b = ClientState
   { clientName :: T.Text,
     msgs :: MessageMap b
   }
@@ -50,9 +55,20 @@ newtype instance StaticRep (Client a b) = Client (ClientState a b)
 
 ----------------------------------------------------------------------------------------------------------
 
+takeOrDefault :: MessageMap b -> UnixMsgTime -> Eff es a
+takeOrDefault messageMap unixTime = runST (do
+  x <- H.lookup messageMap unixTime
+  case x of
+    (Just val) -> return val
+    (Nothing) -> H.insert messageMap unixTime 
+  return undefined
+  )
+
 blockingWaitForResponse :: UnixMsgTime -> Eff es a
-blockingWaitForResponse = do
+blockingWaitForResponse unixTime = do
   (Client (ClientState {msgs})) <- (getStaticRep :: Eff es (StaticRep (Client a b)))
+  x <- runST (H.lookup msgs unixTime)
+  takeMVar x
 
 ask ::
   forall a b c es.
@@ -68,5 +84,5 @@ ask request = do
   unixTime <- unsafeEff_ getUnixTime
   let msgTime = fromUnix unixTime
   write @c msgTime request
-  async ()
+  async undefined
   return undefined
