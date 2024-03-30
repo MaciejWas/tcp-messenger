3eb6a4ffa1f0fc38df662f6382ecc1b7d081e51d
Maciej <wasilewski.maciej20@gmail.com>
2024-03-30 19:39:17 +0100

    migrate to cabal

--- src/TcpMsg/Effects/Client.hs
+++ src/TcpMsg/Effects/Client.hs
@@ -11,22 +11,21 @@
 
 module TcpMsg.Effects.Client where
 
-import Control.Concurrent (ThreadId)
-import Control.Concurrent.Async (Async, async)
-
+import           Control.Concurrent (ThreadId)
+import           Control.Concurrent.Async (Async, async)
+import           Control.Concurrent.STM (atomically)
 import qualified Control.Concurrent.STM as STM
-import qualified Data.ByteString.Lazy as LBS
-import Data.Serialize (Serialize)
-import qualified Data.Text as T
-
-import qualified Network.Socket as Net
-import Data.UnixTime (getUnixTime)
 
 import qualified StmContainers.Map as M
-import TcpMsg.Data (Header (Header), Message, UnixMsgTime, fromUnix)
+
+import           Data.Serialize (Serialize)
+import qualified Data.Text as T
+import           Data.UnixTime (getUnixTime)
+
+import qualified Network.Socket as Net
+
+import TcpMsg.Data (Message, UnixMsgTime, fromUnix)
 import TcpMsg.Effects.Connection (Connection, sendMessage)
-import TcpMsg.Parsing (parseMsg)
-import Control.Concurrent.STM (atomically)
 
 ----------------------------------------------------------------------------------------------------------
 
@@ -77,12 +76,11 @@ blockingWaitForResponse client unixTime = do
 
 ask ::
   forall a b c.
-    Serialize a
-  =>
+  (Serialize a) =>
   Client a b c ->
   Message a ->
   IO (Async (Message b))
-ask client@Client{connection} msg = sendRequest >>= (async . blockingWaitForResponse client)
+ask client@Client {connection} msg = sendRequest >>= (async . blockingWaitForResponse client)
   where
     sendRequest :: IO UnixMsgTime
     sendRequest = do
