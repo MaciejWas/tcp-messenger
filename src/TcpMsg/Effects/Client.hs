ba31ecf20d21aa0ddf501c1f236f208c1a2b057e
Maciej <wasilewski.maciej20@gmail.com>
2024-03-31 10:32:21 +0200

    cabal install hpc-codecov

--- src/TcpMsg/Effects/Client.hs
+++ src/TcpMsg/Effects/Client.hs
@@ -11,19 +11,15 @@
 
 module TcpMsg.Effects.Client where
 
-import           Control.Concurrent (ThreadId)
-import           Control.Concurrent.Async (Async, async)
-import           Control.Concurrent.STM (atomically)
+import Control.Concurrent (ThreadId)
+import Control.Concurrent.Async (Async, async)
+import Control.Concurrent.STM (atomically)
 import qualified Control.Concurrent.STM as STM
-
-import qualified StmContainers.Map as M
-
-import           Data.Serialize (Serialize)
+import Data.Serialize (Serialize)
 import qualified Data.Text as T
-import           Data.UnixTime (getUnixTime)
-
+import Data.UnixTime (getUnixTime)
 import qualified Network.Socket as Net
-
+import qualified StmContainers.Map as M
 import TcpMsg.Data (Message, UnixMsgTime, fromUnix)
 import TcpMsg.Effects.Connection (Connection, sendMessage)
 
